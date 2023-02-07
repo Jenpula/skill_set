7a8de146655d34f2a77cb2d2e9be42a7f9189c77
Annala <n1anje00@students.oamk.fi>
2023-02-07 12:18:40 +0200

    Added state variables and constants

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Slider from '@react-native-community/slider';
+import { useState } from 'react';
+import { Col, Grid } from 'react-native-easy-grid';
+
+
 
 export default function App() {
+
+
+const [value, setValue] = useState(0);
+const [values, setValues] = useState(new Array(SKILLS.length).fill(0));
+const [ average, setAverage] = useState(0);
+
+const SKILLS = ['Frontend', 'Backend', 'Mobile', 'Databases']
+const MIN = 0;
+const MAX = 5;
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text>Value: {value}</Text>
+      <Slider
+        style={{width: 200, height: 40}}
+        minimumValue={0}
+        maximumValue={10}
+        step={1}
+        value={value}
+        minimumTrackTintColor='#FFFFFF'
+        maximumTrackTintColor='#000000'
+        onValueChange={(val) => setValue(val)}
+      />
+      </View>
   );
 }
 
